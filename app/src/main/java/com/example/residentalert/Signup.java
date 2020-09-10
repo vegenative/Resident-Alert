5738d8b1c25081aeca5822dceb5eea05936474aa
pablo <WPawel78@gmail.com>
2020-09-10 11:27:02 +0200

    fix Login layout

--- app/src/main/java/com/example/residentalert/Signup.java
+++ app/src/main/java/com/example/residentalert/Signup.java
@@ -3,13 +3,18 @@ package com.example.residentalert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class Signup extends AppCompatActivity {
 
     private Button signUpBtn;
     private EditText id_et,name_et, surname_et, email_et, password_et, cnfPassword_et, tel_et, street_et, blockNumber_et, city_et, postalCode_et;
+    private ScrollView scrollView;
+    private LinearLayout linearLayout;
 
 
     @Override
@@ -18,7 +23,10 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         signUpBtn = (Button) findViewById(R.id.signup_button);
+
         id_et = (EditText) findViewById(R.id.id_et);
+        name_et = (EditText) findViewById(R.id.name_et);
+        surname_et = (EditText) findViewById(R.id.surname_et);
         email_et = (EditText) findViewById(R.id.email_et);
         password_et = (EditText) findViewById(R.id.password_et);
         cnfPassword_et = (EditText) findViewById(R.id.passwordConfirm_et);
@@ -27,6 +35,19 @@ public class Signup extends AppCompatActivity {
         blockNumber_et = (EditText) findViewById(R.id.blockNumber_et);
         city_et = (EditText) findViewById(R.id.city_et);
         postalCode_et = (EditText) findViewById(R.id.postalCode_et);
+
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+
+        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        scrollView.setVisibility(View.INVISIBLE);
+
 
     }
 }
